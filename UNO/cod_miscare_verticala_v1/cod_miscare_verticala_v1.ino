4470dd7028db4b8e250c2944ff7fcf7cbc5c146c
Radu C <gigacloudmail@gmail.com>
2015-10-16 22:02:00 +0300

    Modificari cod_miscare_verticala_v1

--- UNO/cod_miscare_verticala_v1/cod_miscare_verticala_v1.ino
+++ UNO/cod_miscare_verticala_v1/cod_miscare_verticala_v1.ino
@@ -3,21 +3,28 @@
 //       Cam tot.
 //Neprioritar: Comunicatie intre Mega si UNO
 int ok, ch2, ch4, m2, m4;
-int pinCanal2 = 12;
-int pinCanal4 = 13;
-int pinReleu = 14;
-int vertical_max, vertical_centru_max;
-int vertical_min, vertical_centru_min;
-int fata_max, fata_centru_max;
-int fata_min , fata_centru_min;
+const int pinCanal2 = 12;
+const int pinCanal4 = 13;
+const int pinCanal5 = 14;
+const int pinReleu = 15;
+const int motorSJ1 = 16;
+const int motorSJ2 = 17;
+const int motorSD1 = 18;
+const int motorSD2 = 19;
+int vertical_max, vertical_centru;
+int vertical_min;
+int fata_max, fata_centru;
+int fata_min;
 int m2_pwm_sus, m2_pwm_jos;
+bool doarUnMotor = false;
 
 void setup() {
 
   pinMode(pinCanal2, INPUT); //input canal 2
   pinMode(pinCanal4, INPUT); //input canal 4
-  pinMode(motor1, OUTPUT); //output motor1_a
-  pinMode(motor2, OUTPUT); //output motor 2_a
+  pinMode(pinCanal5, INPUT); //input canal 5
+  pinMode(motorSJ1, OUTPUT); //output motorSJ1
+  pinMode(motorSJ2, OUTPUT); //output motor SJ2
   ok = false; //bool medie
   Serial.begin(19200); //baud-rate
 
@@ -43,14 +50,24 @@ void pwmDesfacere(){
 
 }
 
+void pwmFata(){
+//Matematica care ne bate aici
+
+}
+
+void pwmSpate(){
+//Matematica care ne bate aici
+
+}
 void citire_medie() {
-   m3 = pulseIn(pinCanal2,HIGH,15000); //citire canal 2 telecomanda
-   m1 = pulseIn(pinCanal4, HIGH, 15000); //citire canal 4 telecomanda
+   m2 = pulseIn(pinCanal2, HIGH,15000); //citire canal 2 telecomanda
+   m4 = pulseIn(pinCanal4, HIGH, 15000); //citire canal 4 telecomanda
+   ch5 = pulseIn(pinCanal5, HIGH, 15000);//citire canal 5 telecomanda
    for (int c=1;c<=10;c++) {
      ch3 = pulseIn(pinCanal2,HIGH,15000);
      m3 = (m4 + ch4) / 2;
-     ch1 = pulseIn(pinCanal4,HIGH,15000);
-     m1 = (m2 + ch2) / 2;
+     ch2 = pulseIn(pinCanal4,HIGH,15000);
+     m2 = (m2 + ch2) / 2;
    }
 
    ok = true;
@@ -60,13 +77,15 @@ void citire_medie() {
 
 
 void miscareSus() {
-  analogWrite(motor1, m2_pwm_sus);
-  analogWrite(motor2, m2_pwm_sus);
+  analogWrite(motorSJ1, m2_pwm_sus);
+  if(!doarUnMotor)
+    analogWrite(motorSJ2, m2_pwm_sus);
 }
 
 void miscareJos() {
-  analogWrite(motor1, m2_pwm_jos);
-  analogWrite(motor2, m2_pwm_jos);
+  analogWrite(motorSJ1, m2_pwm_jos);
+  if(!doarUnMotor)
+    analogWrite(motorSJ2, m2_pwm_jos);
 }
 
 void miscareStrangere() {
@@ -121,6 +140,11 @@ bool centrat(){
 }
 
 void principal() {
+  citire_medie();
+  if(ch5 > 1500)
+    doarUnMotor = true;
+  else doarUnMotor = false;
+
   if ( !centrat() ) {
     comandaSus();
     comandaJos();
