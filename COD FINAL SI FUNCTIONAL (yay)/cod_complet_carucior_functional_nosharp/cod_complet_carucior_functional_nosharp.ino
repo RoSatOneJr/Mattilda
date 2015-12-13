3c4c78c65a719dda014d3a47e6aee76f7078ec67
unknown <constantin22.2.01@gmail.com>
2015-12-13 20:42:22 +0200

    Finally done

--- COD FINAL SI FUNCTIONAL (yay)/cod_complet_carucior_functional_nosharp/cod_complet_carucior_functional_nosharp.ino
+++ COD FINAL SI FUNCTIONAL (yay)/cod_complet_carucior_functional_nosharp/cod_complet_carucior_functional_nosharp.ino
@@ -1,7 +1,6 @@
-const int pinCanal4 = 42; //Fata-Spate
-const int pinCanal5 = 44; //Comanda pate!
-const int pinCanal7 = 46; //Mod doar un motor
-const int pinCanal8 = 48; //Switch Strangere-Desfacere
+const int pinCanal4 = 19; //Fata-Spate
+const int pinCanal5 = 20; //Comanda pate!
+const int pinCanal8 = 21; //Switch Strangere-Desfacere
 
 const int motorSD1_a = 10;//stanga
 const int motorSD1_b = 11;
@@ -20,19 +19,17 @@ const int senzor_fata_spate = A0;
 
 const int pinSwitchStanga = 52;
 const int pinSwitchDreapta = 50;
-int ch4, m4, ch5, ch8, ch7, citireSwitch;
+
+int m4, ch5, ch8,citireSwitch;
 int strans = false, maxim_fata = false, maxim_spate = false;
-String ip = "192.168.1.8";
-long last_time_esp;
+int timp4, timp5, timp7, timp8;
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial3.begin(9600);
   pinMode(pinCanal4, INPUT);
   pinMode(pinCanal5, INPUT);
-  pinMode(pinCanal7, INPUT);
   pinMode(pinCanal8, INPUT);
   pinMode(pinSwitchStanga, INPUT);
   pinMode(pinSwitchDreapta, INPUT);
@@ -55,55 +52,12 @@ void setup() {
   digitalWrite(motorFS2_a, LOW);
   digitalWrite(motorFS2_b, LOW);
   
+    
+  attachInterrupt( digitalPinToInterrupt( pinCanal4 ), citire4, CHANGE );
+  attachInterrupt( digitalPinToInterrupt( pinCanal5 ), citire5, CHANGE );
+  attachInterrupt( digitalPinToInterrupt( pinCanal8 ), citire8, CHANGE );
   
 }
-
-
-void ESP2(){
- if(millis() - last_time_esp > 500){
-  int sharp1 = analogRead(A1);
-  int sharp2 = analogRead(A2);
-  char buf[16];
-  String sharp1_string = dtostrf(sharp1, 4, 1, buf);
-  String sharp2_string = dtostrf(sharp2, 4, 1, buf);
-  
-  // TCP connection
-  String cmd = "AT+CIPSTART=\"TCP\",\"";
-  cmd += ip; // Serveru' de pe laptop
-  cmd += "\",80";
-  Serial3.println(cmd);
-   
-  if(Serial3.find("Error")){
-    Serial.println("AT+CIPSTART error");
-    return;
-  }
-  
-  // prepare GET string
-  String getStr = "GET /esp2.php?date1=";
-  getStr += String(sharp1_string);
-  getStr += "&date2=";
-  getStr += String(sharp2_string);
-  getStr += "\r\n\r\n";
-
-  // send data length
-  cmd = "AT+CIPSEND=";
-  cmd += String(getStr.length());
-  Serial3.println(cmd);
-
-  if(Serial3.find(">")){
-    Serial3.print(getStr);
-  }
-  else{
-    Serial3.println("AT+CIPCLOSE");
-    // alert user
-    Serial.println("AT+CIPCLOSE");
-    last_time_esp = millis();
-  }
-  // delay(500); VA TREBUI REZOLVAT
- }
-}
-
-
 
 int Switch(int brat) { //0-stanga; 1-dreapta
   if(brat == 0) {citireSwitch = digitalRead(pinSwitchStanga);}
@@ -123,12 +77,11 @@ void Desfacere(){
     }
 
     if(Switch(0) == 1){
-
+    Serial.print("###############################################################");
     digitalWrite(motorSD1_a, HIGH);
     digitalWrite(motorSD1_b, HIGH);
     }
 
-  if(ch7 > 1500){
     Serial.println("MOTOR 2");
     if(Switch(1) == 0){
       digitalWrite(motorSD2_a, LOW);
@@ -139,14 +92,13 @@ void Desfacere(){
       digitalWrite(motorSD2_a, HIGH);
       digitalWrite(motorSD2_b, HIGH);
     }
-  }
 }
 
 void Strangere(){
 
    Serial.print("Strangere \n\n");
 
-   if(analogRead(senzor_brate) > 480 && strans == false) {
+   if(analogRead(senzor_brate) > 470 && strans == false) {
 
     digitalWrite(motorSD1_a, HIGH);
     digitalWrite(motorSD1_b, LOW);
@@ -154,9 +106,8 @@ void Strangere(){
     digitalWrite(motorSD2_a, HIGH);
     digitalWrite(motorSD2_b, LOW);
     }
-  if(ch7 > 1500){
-    Serial.println("MOTOR 2");
-   if(analogRead(senzor_brate) < 475 && strans == false) {
+
+   if(analogRead(senzor_brate) < 470 && strans == false) {
     strans = true;
     digitalWrite(motorSD1_a, HIGH);
     digitalWrite(motorSD1_b, HIGH);
@@ -164,7 +115,6 @@ void Strangere(){
     digitalWrite(motorSD2_a, HIGH);
     digitalWrite(motorSD2_b, HIGH);
    }
-  }
 
 }
 
@@ -179,70 +129,45 @@ void Scurt_brate(){
 
 void Fata(){
    Serial.println("Fata");
-   //if((analogRead(senzor_fata_spate) > 460) && maxim_fata == false){
-   if(m4 < 1750){
-     analogWrite(motorFS1_a, 200);
-     digitalWrite(motorFS1_b, LOW);
-     analogWrite(motorFS2_a, 200);
-     digitalWrite(motorFS2_b, LOW);
-     maxim_spate = false;
-     
-   }
-   
+   if((analogRead(senzor_fata_spate) > 460) && maxim_fata == false){
+    
+   digitalWrite(motorFS1_a, HIGH);
+   digitalWrite(motorFS1_b, LOW);
+   digitalWrite(motorFS2_a, HIGH);
+   digitalWrite(motorFS2_b, LOW);
+   maxim_spate = false;
+  }
 
-    if(m4 > 1751){  
-     digitalWrite(motorFS1_a, HIGH);
-     digitalWrite(motorFS1_b, LOW);
-     digitalWrite(motorFS2_a, HIGH);
-     digitalWrite(motorFS2_b, LOW);
-     maxim_spate = false;
-
-   }
-  //}  
-//}
-
-// if((analogRead(senzor_fata_spate)  < 460) && maxim_fata == false){
-
-     digitalWrite(motorFS1_a, HIGH);
-     digitalWrite(motorFS1_b, HIGH);
-     digitalWrite(motorFS2_a, HIGH);
-     digitalWrite(motorFS2_b, HIGH);
-     maxim_fata = true;
-// }
+ if((analogRead(senzor_fata_spate)  < 460) && maxim_fata == false){
+   digitalWrite(motorFS1_a, HIGH);
+   digitalWrite(motorFS1_b, HIGH);
+   digitalWrite(motorFS2_a, HIGH);
+   digitalWrite(motorFS2_b, HIGH);
+   maxim_fata = true;
+ }
   
 }
 
 void Spate(){
    Serial.println("Spate");
 
-   //if((analogRead(senzor_fata_spate) > 460) && maxim_spate == false){
-     if(m4 > 1250){
-       digitalWrite(motorFS1_a, LOW);
-       analogWrite(motorFS1_b, 200);
-       digitalWrite(motorFS2_a, LOW);
-       analogWrite(motorFS2_b, 200);
-       maxim_fata = false;
-       
+   if((analogRead(senzor_fata_spate) > 460) && maxim_spate == false){
+    
+   digitalWrite(motorFS1_a, LOW);
+   digitalWrite(motorFS1_b, HIGH);
+   digitalWrite(motorFS2_a, LOW);
+   digitalWrite(motorFS2_b, HIGH);
+   maxim_fata = false;
+  }
 
-    }
-    if(m4 < 1250){
-
-       digitalWrite(motorFS1_a, LOW);
-       digitalWrite(motorFS1_b, HIGH);
-       digitalWrite(motorFS2_a, LOW);
-       digitalWrite(motorFS2_b, HIGH);
-       maxim_fata = false;
-     }
-//  }
-
-// if((analogRead(senzor_fata_spate) < 460 ) && maxim_spate == false){
+ if((analogRead(senzor_fata_spate) < 460 ) && maxim_spate == false){
 
    digitalWrite(motorFS1_a, HIGH);
    digitalWrite(motorFS1_b, HIGH);
    digitalWrite(motorFS2_a, HIGH);
    digitalWrite(motorFS2_b, HIGH); 
    maxim_spate = true;
-  //} 
+  } 
 }
 
 void Scurt_fata_spate(){
@@ -253,19 +178,7 @@ void Scurt_fata_spate(){
    digitalWrite(motorFS2_b, HIGH);
 }
 
-void Citire(){
-   ch4 = pulseIn(pinCanal4,HIGH,15000);
-   m4 = ch4;
-
-   ch5 = pulseIn(pinCanal5, HIGH, 15000);
-   ch7 = pulseIn(pinCanal7, HIGH, 15000);
-   ch8 = pulseIn(pinCanal8, HIGH, 15000);
-}
-
-
 void loop() {
-  Citire();
-//  ESP2();
   if(ch5  > 1200){
     
     if(ch8 < 1500){ Desfacere();}
@@ -273,14 +186,50 @@ void loop() {
    }
   if(ch5 < 1200 ) {Scurt_brate();}
   
-  if(m4 > 1550) { Fata();}
-  if(m4 < 1350) { Spate();}
-  if(m4 > 1350 && m4 < 1750) {Scurt_fata_spate();}
+  if(m4 > 1700) { Fata();}
+  if(m4 < 1250) { Spate();}
+  if(m4 > 1300 && m4 < 1700) {Scurt_fata_spate();}
+ /* Serial.print("A0: ");Serial.print(analogRead(A0));Serial.print("\n");
+  Serial.print("A1: ");Serial.print(analogRead(A1));Serial.print("\n");
+  Serial.print("A2: ");Serial.print(analogRead(A2));Serial.print("\n");*/
+  Serial.print("canal5 ");
+ Serial.println(ch5);
+ Serial.print("canal4 ");
   Serial.println(m4);
- //Serial.println(ch5);
-  //Serial.println(m4);
- // Serial.println(ch7);
+  Serial.print("canal8 ");
+  Serial.println(ch8);
  //   Serial.println("##############");
 //    Serial.print("Senzor: ");Serial.print(analogRead(senzor));Serial.print("\n");
 
 }
+
+void citire4(){
+  
+  if(digitalRead( pinCanal4 ) == HIGH){
+    timp4 = micros();
+  }
+  else{
+    m4 = micros() - timp4;
+  }
+}
+
+void citire5(){
+  
+  if(digitalRead( pinCanal5 ) == HIGH){
+    timp5 = micros();
+  }
+  else{
+    ch5 = micros() - timp5;
+  }
+}
+
+void citire8(){
+  
+  if(digitalRead( pinCanal8 ) == HIGH){
+    timp8 = micros();
+  }
+  else{
+    ch8 = micros() - timp8;
+  }
+}
+
